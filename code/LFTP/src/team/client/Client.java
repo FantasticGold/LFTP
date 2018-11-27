488af9d4062820ba4004dc2c13b54877aed75544
Blasticag <2793175942@qq.com>
2018-11-27 10:52:58 +0800

    Start Client

--- code/LFTP/src/team/client/Client.java
+++ code/LFTP/src/team/client/Client.java
@@ -1,5 +1,13 @@
 package team.client;
 
 public class Client {
-
+	public Client() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("test");
+		Client client = new Client();
+	}
+	
 }
