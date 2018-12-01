75e42d0a896c609e5838e5cd0d283608eb3ac033
Blasticag <2793175942@qq.com>
2018-12-01 22:34:35 +0800

    Reader和Writer函数接口变动

--- code/LFTP/src/team/LFTP/Reader.java
+++ code/LFTP/src/team/LFTP/Reader.java
@@ -1,12 +1,50 @@
 package team.LFTP;
 
+import java.io.File;
+import java.io.FileInputStream;
+
 public class Reader {
+	long remainLength = 0;
+	File file = null;
+	FileInputStream fileInputStream = null;
+	private boolean Opened = false;
   public Reader(String name) {
-    
+    try {
+    	file = new File(name);
+    	if (file.exists() && file.isFile()) {
+    		fileInputStream = new FileInputStream(file);
+    		Opened = true;
+    		remainLength = file.length();
+    	}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
   }
   
   // null to EOF, return actual length
-  byte[] read(int len) {
-    return null;
+  byte[] read(long len) {
+  	try {
+			if (len <= 0 || !isOpen()) return null;
+			
+			long dataLength = remainLength < len ? remainLength:len;
+			if (dataLength == 0) return null;
+			byte[] data = new byte[(int) dataLength];
+			fileInputStream.read(data);
+			remainLength -= dataLength;
+			
+			//If reach the end, close fileInputStream
+			if (remainLength == 0) {
+				fileInputStream.close();
+				Opened = false;
+			}
+			return data;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+  	return null;
   }
+  
+  public boolean isOpen() {
+		return Opened;
+	}
 }
--- code/LFTP/src/team/LFTP/Writer.java
+++ code/LFTP/src/team/LFTP/Writer.java
@@ -1,10 +1,33 @@
 package team.LFTP;
 
+import java.io.File;
+import java.io.FileOutputStream;
+
 public class Writer {
+	File file;
+	FileOutputStream fileOutputStream;
   public Writer(String name) {
-    
+  	try {
+			file = new File(name);
+			fileOutputStream = new FileOutputStream(file);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
   }
+  
   public void write(byte[] data) {
-    
+  	try {
+			fileOutputStream.write(data);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
   }
+  
+  public void closeFileStream() {
+		try {
+			fileOutputStream.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
