833bbcdcead2b5f3dd478e2ec9385ccd51d155e4
Blasticag <2793175942@qq.com>
2018-12-02 00:18:52 +0800

    函数参数改变

--- code/LFTP/src/team/LFTP/Writer.java
+++ code/LFTP/src/team/LFTP/Writer.java
@@ -6,11 +6,13 @@ import java.io.FileOutputStream;
 public class Writer {
 	File file;
 	FileOutputStream fileOutputStream;
+	long requiredLength = 0;
 	
-  public Writer(String name) {
+  public Writer(String name, long len) {
   	try {
 			file = new File(name);
 			fileOutputStream = new FileOutputStream(file);
+			requiredLength = len;
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -18,24 +20,25 @@ public class Writer {
   
   public void write(byte[] data) {
   	try {
-			fileOutputStream.write(data);
+  		if ((long) data.length >= requiredLength) {
+  			fileOutputStream.write(data, 0, (int) requiredLength);
+  			requiredLength = 0;
+  			fileOutputStream.close();
+  		}
+  		else {
+  			fileOutputStream.write(data);
+  			requiredLength -= (long) data.length;
+  		}
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
   }
-  
-  public void closeFileStream() {
-		try {
-			fileOutputStream.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
 
   // test
   public static void main(String[] args) {
-    Writer writer = new Writer("F:\\SYSU_3.1\\test.txt");
-    Reader reader = new Reader("F:\\SYSU_3.1\\hello.txt");
+    Writer writer = new Writer("C:\\Users\\czj\\Desktop\\hello2.txt", 10);
+    Reader reader = new Reader("C:\\Users\\czj\\Desktop\\hello.txt");
     while (reader.isOpen()) {
       byte[] data = reader.read(20);
       writer.write(data);
